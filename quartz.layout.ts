af6d67f0e2262eb1550345df720edf30562a9b7f
KMAYAMA2 <c191246x@gmail.com>
2024-07-18 06:57:34 +0200

    Recent Notes added

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -29,7 +30,34 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        title: "Explore", // title of the explorer component
+        folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+        folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
+        useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+        // Sort order: folders first, then files. Sort folders and files alphabetically
+        /*
+        sortFn: (a, b) => {
+          ... // default implementation shown later
+        },
+        filterFn: filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+        mapFn: undefined,
+        // what order to apply functions in
+        order: ["filter", "map", "sort"],
+        */
+      }),
+    ),
+    Component.DesktopOnly(
+      Component.RecentNotes({
+        title: "Recent Notes",
+        limit: 3,
+        showTags: false,
+        filter: (f) =>
+          f.slug!.startsWith("notes/") && f.slug! !== "notes/index" && !f.frontmatter?.noindex,
+        linkToMore: "notes/" as SimpleSlug,
+      }),
+    ),
   ],
   right: [
     Component.Graph(),
