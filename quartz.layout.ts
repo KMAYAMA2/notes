178a4c5d34e5c9f0738e18260e4f2772a7f47238
kmayama2 <130614867+KMAYAMA2@users.noreply.github.com>
2024-08-12 11:02:42 +0900

    Layout update
    Explorer set to collapsed by default

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,7 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
         title: "Explore", // title of the explorer component
         folderClickBehavior: "collapse", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
         folderDefaultState: "collapsed", // default state of folders ("collapsed" or "open")
-        useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+        useSavedState: false, // whether to use local storage to save "state" (which folders are opened) of explorer
         // Sort order: folders first, then files. Sort folders and files alphabetically
         /*
         sortFn: (a, b) => {
