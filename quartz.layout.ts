8210e6f6e82923cf59d3aa7eb6c7982630c77b9f
kmayama2 <130614867+KMAYAMA2@users.noreply.github.com>
2025-05-25 07:44:53 +0900

    showTags: false

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -95,7 +95,7 @@ export const defaultContentPageLayout: PageLayout = {
         fontSize: 0.6, // what size should the node labels be?
         opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
-        showTags: true, // whether to show tags in the graph - was false
+        showTags: false, // whether to show tags in the graph - was false
         // enableRadial: false, // whether to constrain the graph, similar to Obsidian
       },
       globalGraph: {
